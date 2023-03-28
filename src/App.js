3b41c2fea8979afdd1c497e0c1f269f7465c7d69
normanlikescats <normankwok97@gmail.com>
2023-03-28 15:11:30 +0800

    display results on front end and ability to click into sightings

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import logo from "./logo.png";
 import "./App.css";
 
-class App extends React.Component {
-  render() {
-    return (
+const URL = "http://localhost:8080"
+
+function App () {
+  const [sightings, setSightings] = useState([]);
+  const [detailMode, setDetailMode] = useState(false);
+  const [sightingDeets, setSightingDeets] = useState(false);
+  
+  useEffect(()=>{
+    axios.get(`${URL}/sightings`).then((data)=>{
+      console.log(data)
+      setSightings(data.data);
+    })
+  }, [])
+
+  function handleClick(e){
+    axios(`${URL}/sightings/${e}`).then((data)=>{
+      setSightingDeets(data.data)
+      setDetailMode(true)
+    })
+  }
+
+  let sightingsItems = sightings.map((item, counter)=>{
+    return(<p onClick={()=>{handleClick(counter)}}>{item.YEAR} - {item.SEASON} - {item.STATE}</p>)
+  })
+
+  let sightingDeetsItems = (
+    <div>
+      <button onClick={()=>setDetailMode(!detailMode)}>Back</button>
+      <p>
+        Year: {sightingDeets.YEAR} <br/>
+        Season: {sightingDeets.SEASON} <br/>
+        Time and Conditions: {sightingDeets.TIME_AND_CONDITIONS} <br/>
+        State: {sightingDeets.STATE} <br/>
+        County: {sightingDeets.COUNTY} <br/>
+        Location Details: {sightingDeets.LOCATION_DETAILS} <br/>
+        Observed: {sightingDeets.OBSERVED} <br/>
+        Other Witnesses: {sightingDeets.OTHER_WITNESSES} <br/>
+        Report Class: {sightingDeets.REPORT_CLASS} <br/>
+        Report Number: {sightingDeets.REPORT_NUMBER} <br/>
+      </p>
+    </div> 
+  )
+
+
+  return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {detailMode
+            ? sightingDeetsItems
+            : sightingsItems}
           </p>
         </header>
       </div>
-    );
-  }
+  );
 }
+
 
 export default App;
